621a9c2b19286c1bc10a0e300f0f2fab1ab27ea5
Dao Quoc Khanh <khanhdq2411@gmail.com>
2025-02-12 08:53:30 +0700

    update code

--- package/next.config.mjs
+++ package/next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
+    async rewrites() {
+        return [
+            {
+                source: "/:path*",  // Khi FE gọi `/api/...`, nó sẽ được chuyển tiếp sang BE
+                destination: "http://localhost:8080/:path*",
+            },
+        ];
+    },
 };
 
 export default nextConfig;
